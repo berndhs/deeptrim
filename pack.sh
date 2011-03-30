588ed3316186dd448da80479fbc2b61b3f3aec8b
Bernd Stramm <bernd.stramm@gmail.com>
2011-03-29 21:16:10 -0400

    update packaging

--- pack.sh
+++ pack.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 EXE_NAME=deeptrim
 PROJ_NAME=permute
+CHANGELOG=${EXE_NAME}.changes
+DESKTOP=${EXE_NAME}.desktop
 VERSION=`grep "ProgramVersion::VersionNumber" src/version.cpp \
         | awk '{print $3;}' \
         | sed s/[\(\"\;\)]//g`
@@ -9,6 +10,8 @@ PACKDIR=${HOME}/packaging/${PROJ_NAME}
 
 makearchive.sh ${EXE_NAME}-${VERSION} master
 cp ${EXE_NAME}-${VERSION}.tar.gz ${PACKDIR}
+cp ${CHANGELOG} ${PACKDIR}
+cp ${DESKTOP} ${PACKDIR}
 echo ${EXE_NAME} > ${PACKDIR}/pack-name
 echo ${VERSION} > ${PACKDIR}/pack-version
 ls -l ${PACKDIR}/${EXE_NAME}-${VERSION}.tar.gz
@@ -19,3 +22,4 @@ then
   cd ${PACKDIR}
   make
 fi
+
